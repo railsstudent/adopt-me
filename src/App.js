a925ff9adf03b66c85f10f376147bba09df8b937
Connie Leung <cleung334@gmail.com>
2020-06-24 08:44:32 +0800

    chore: setup prettier
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-const Pet = ({name, breed, animal}) => React.createElement("div", {}, 
-    [
-        React.createElement("h1", {}, name),
-        React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
-    ]
-);
+const Pet = ({ name, breed, animal }) =>
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed),
+  ]);
 
-const App = () => React.createElement("div", {}, [
-    React.createElement("h1", { id: 'x' }, "Adopt me!"),
-    React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanese' }),
-    React.createElement(Pet, { name: 'Pepper', animal: 'Bird', breed: 'Cockatiel' }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
-]);
+const App = () =>
+  React.createElement("div", {}, [
+    React.createElement("h1", { id: "x" }, "Adopt me!"),
+    React.createElement(Pet, {
+      name: "Luna",
+      animal: "Dog",
+      breed: "Havanese",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      animal: "Bird",
+      breed: "Cockatiel",
+    }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" }),
+  ]);
 
-ReactDOM.render( React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
