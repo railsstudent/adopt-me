eba5ff0fe18776b13e8e794d445d8afc5b616822
Connie Leung <cleung334@gmail.com>
2020-06-24 08:36:11 +0800

    render multiple Pet components
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-const Pet = () => React.createElement("div", {}, 
+const Pet = ({name, breed, animal}) => React.createElement("div", {}, 
     [
-        React.createElement("h1", {}, "Luna"),
-        React.createElement("h2", {}, "Dog"),
-        React.createElement("h2", {}, "Havana")
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
     ]
 );
 
 const App = () => React.createElement("div", {}, [
     React.createElement("h1", { id: 'x' }, "Adopt me!"),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet)
+    React.createElement(Pet, { name: 'Luna', animal: 'Dog', breed: 'Havanese' }),
+    React.createElement(Pet, { name: 'Pepper', animal: 'Bird', breed: 'Cockatiel' }),
+    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mix" })
 ]);
 
 ReactDOM.render( React.createElement(App), document.getElementById("root"));
