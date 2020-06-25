390942c5fd5c029cf848af9f79628dc818f9f66d
Connie Leung <cleung334@gmail.com>
2020-06-25 15:48:37 +0800

    feat: searchParam component
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParam'
 
 const App = () => (
   <div>
     <h1 id="something-import">Adopt me!</h1>
-    <Pet key="pet1" name="Luna" animal="Dog" breed="Havanese"></Pet>
-    <Pet key="pet2" name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-    <Pet key="pet3" name="Doink" animal="Cat" breed="Mix"></Pet>
+    <SearchParams></SearchParams>
   </div>
 );
 
---
+++ src/SearchParam.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SearchParams = () => {
+    const location = 'Seattle, WA';
+    return (
+      <div className="search-params">
+        <form >
+            <label htmlFor="location">
+                Location
+                <input id="location" value={location} placeholder="Location"></input>
+            </label>
+            <button>Submit</button>
+        </form>
+      </div>
+    );
+  }
+  
+export default SearchParams
