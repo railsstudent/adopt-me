61dd6dbe3461d7105c14101e4dcdb56badd47310
Connie Leung <cleung334@gmail.com>
2020-06-25 13:54:03 +0800

    configure eslint for React
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Pet from './Pet'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Pet from './Pet';
 
-const App = () =>
-  (
-    <div>
-      <h1 id="something-import">Adopt me!</h1>
-      <Pet key="pet1" name="Luna" animal="Dog" breed="Havanese"></Pet>
-      <Pet key="pet2" name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
-      <Pet key="pet3" name="Doink" animal="Cat" breed="Mix"></Pet>
-    </div>
-  )
+const App = () => (
+  <div>
+    <h1 id="something-import">Adopt me!</h1>
+    <Pet key="pet1" name="Luna" animal="Dog" breed="Havanese"></Pet>
+    <Pet key="pet2" name="Pepper" animal="Bird" breed="Cockatiel"></Pet>
+    <Pet key="pet3" name="Doink" animal="Cat" breed="Mix"></Pet>
+  </div>
+);
 
 ReactDOM.render(React.createElement(App), document.getElementById('root'));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
-export default function Pet ({ name, breed, animal }) {
+export default function Pet({ name, breed, animal }) {
   return (
     <div>
-        <h1>{name}</h1>
-        <h2>{animal}</h2>
-        <h2>{breed}</h2>
+      <h1>{name}</h1>
+      <h2>{animal}</h2>
+      <h2>{breed}</h2>
     </div>
-  )
+  );
 }
