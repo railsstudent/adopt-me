fb112880960efafc8f01e1a0bdcbc8607010e06a
Connie Leung <cleung334@gmail.com>
2020-06-30 08:39:59 +0800

    feat: use useState to update location
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/SearchParam.js
+++ src/SearchParam.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ANIMALS } from '@frontendmasters/pet'
 
 const SearchParams = () => {
-    const location = 'Seattle, WA';
+    const [location, setLocation] = useState('Seattle, WA');
     return (
       <div className="search-params">
+        <h1>{location}</h1>
         <form >
             <label htmlFor="location">
                 Location
-                <input id="location" value={location} placeholder="Location"></input>
+                <input id="location" value={location} 
+                  placeholder="Location" 
+                  onChange={e => setLocation(e.target.value)}>
+                </input>
             </label>
             <button>Submit</button>
         </form>
