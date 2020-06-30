fad417d66f32b410ec2b6f063eb2245176b9cb58
Connie Leung <cleung334@gmail.com>
2020-06-30 09:14:04 +0800

    feat: add animal dropdown
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/SearchParam.js
+++ src/SearchParam.js
@@ -2,10 +2,15 @@ import React, { useState } from 'react'
 import { ANIMALS } from '@frontendmasters/pet'
 
 const SearchParams = () => {
+    console.log('ANIMALS', ANIMALS)
+    
     const [location, setLocation] = useState('Seattle, WA');
+    const [animal, setAnimal] = useState('');
+
     return (
       <div className="search-params">
         <h1>{location}</h1>
+        <h2>{animal}</h2>
         <form >
             <label htmlFor="location">
                 Location
@@ -14,6 +19,20 @@ const SearchParams = () => {
                   onChange={e => setLocation(e.target.value)}>
                 </input>
             </label>
+            <label htmlFor="animal">
+                Animal
+                <select  
+                  id="animal" 
+                  value={animal}
+                  onChange={e => setAnimal(e.target.value)}
+                  onBlur={e => setAnimal(e.target.value)}
+                >
+                  <option value="">All</option>
+                  {ANIMALS.map(animal => <option key={animal} value={animal}
+                  >{animal}</option>)}
+                </select>
+            </label>
+            
             <button>Submit</button>
         </form>
       </div>
