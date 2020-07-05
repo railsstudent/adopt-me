e04d0eeab0cb4d942f9d3f43d0eeee8ad17c7556
Connie Leung <cleung334@gmail.com>
2020-07-05 23:21:52 +0800

    refactor:  custom dropdown

--- src/SearchParam.js
+++ src/SearchParam.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import { ANIMALS } from '@frontendmasters/pet'
+import useDropdown from './useDropdown';
 
 const SearchParams = () => {
     console.log('ANIMALS', ANIMALS)
     
     const [location, setLocation] = useState('Seattle, WA');
-    const [animal, setAnimal] = useState('');
-    const [breed, setBreed] = useState('');
     const [breeds, setBreeds] = useState([]);
+    const [animal, AnimalDropdown] = useDropdown('Animal', 'dog', ANIMALS);
+    const [breed, BreedDropdown] = useDropdown('Breed', '', breeds);
     
     return (
       <div className="search-params">
@@ -21,32 +22,8 @@ const SearchParams = () => {
                   onChange={e => setLocation(e.target.value)}>
                 </input>
             </label>
-            <label htmlFor="animal">
-                Animal
-                <select  
-                  id="animal" 
-                  value={animal}
-                  onChange={e => setAnimal(e.target.value)}
-                  onBlur={e => setAnimal(e.target.value)}
-                >
-                  <option value="">All</option>
-                  {ANIMALS.map(animal => <option key={animal} value={animal}
-                  >{animal}</option>)}
-                </select>
-            </label>
-            <label htmlFor="breed">
-                Breed
-                <select  
-                  id="breed" 
-                  value={breed}
-                  onChange={e => setBreed(e.target.value)}
-                  onBlur={e => setBreed(e.target.value)}
-                >
-                  <option value="">All</option>
-                  {breeds.map(breedStr => <option key={breedStr} value={breedStr}
-                  >{breedStr}</option>)}
-                </select>
-            </label>
+            <AnimalDropdown />
+            <BreedDropdown />
             <button>Submit</button>
         </form>
       </div>
