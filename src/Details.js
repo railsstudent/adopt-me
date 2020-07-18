d00b50b9bdc434fdb1fcd09f21e041458eefce96
Connie Leung <cleung334@gmail.com>
2020-07-18 20:13:02 +0800

    feat: display modal

--- src/Details.js
+++ src/Details.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect, useContext } from 'react';
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ThemeContext from './ThemeContext';
+import Modal from './Modal';
+import { navigate } from '@reach/router';
 
 const Details = (props) => {
     const id = +props.id;
     const [details, setDetails] = useState(null);
-    const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true);
     const [{ backgroundColor, color }] = useContext(ThemeContext);
+    const [showModal, setShowModal] = useState(false);
+    const [url, setUrl] = useState('')
 
     useEffect(() => {
         setDetails(null)
@@ -18,7 +22,8 @@ const Details = (props) => {
                     contact: { 
                         address: { city, state }
                     },
-                    breeds: { primary: breed } 
+                    breeds: { primary: breed },
+                    url 
                 } = animal || { 
                     name: '', 
                     description: '',
@@ -31,7 +36,8 @@ const Details = (props) => {
                     },
                     breeds: {
                         primary: ''
-                    }
+                    },
+                    url: ''
                 };
                 setDetails({
                     name,
@@ -41,9 +47,13 @@ const Details = (props) => {
                     media,
                     breed,
                 });
+                setUrl(url)
                 setLoading(false);
             }, console.error)
     }, [id]);
+
+    const toggleModal = () => setShowModal(!showModal);
+    const adopt = () => navigate(url);
 
     return (
         loading ? <h1>Loading...</h1> :
@@ -52,8 +62,21 @@ const Details = (props) => {
                 <div>
                     <h1>{details.name}</h1>
                     <h2>{`${details.animal} - ${details.breed} - ${details.location}`}</h2>
-                    <button style={{ backgroundColor, color }}>Adopt {details.name}</button>
+                    <button style={{ backgroundColor, color }} onClick={toggleModal}>Adopt {details.name}</button>
                     <p>{details.description}</p>
+                    {
+                        showModal ?
+                        <Modal>
+                            <div>
+                                <h1>Would you like to adopt {details.name}?</h1>
+                                <div className="buttons">
+                                    <button onClick={adopt}>Yes</button>
+                                    <button onClick={toggleModal}>No</button>
+                                </div>
+                            </div>
+                        </Modal>                        
+                        : null
+                    }
                 </div>
             </div>
     )
